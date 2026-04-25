1254db13e23177b3943394a97517a580d1d1ad15
Sima Bagheri <37793675+simaba@users.noreply.github.com>
2026-04-25 20:31:27 +0800

    Add pmkit package init

---
+++ src/pmkit/__init__.py
@@ -0,0 +1,5 @@
+"""Lightweight validation utilities for Everything Program Management."""
+
+from .validators import validate_json_file, validate_markdown_frontmatter
+
+__all__ = ["validate_json_file", "validate_markdown_frontmatter"]
